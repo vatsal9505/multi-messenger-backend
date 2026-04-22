1daf985d7cb8256be6cc4ea78052261dc18cfd45
Vatsal Kurhade <stonedelmo09@gmail.com>
2026-04-22 20:16:06 +0530

    Added home endpoint

--- src/main/java/com/project/multimessenger/controller/MessageController.java
+++ src/main/java/com/project/multimessenger/controller/MessageController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.multimessenger.dto.MessageRequest;
@@ -17,20 +16,23 @@ import com.project.multimessenger.service.MessageService;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:5173")
-@RequestMapping("/message")   // 🔥 better API structure
 public class MessageController {
 
     @Autowired
     private MessageService messageService;
+
+    @GetMapping("/")
+    public String home() {
+        return "MultiMessenger Backend Live";
+    }
 
     @GetMapping("/test")
     public String test() {
         return "Backend is working";
     }
 
-    @PostMapping("/send")
+    @PostMapping("/sendMessage")
     public ResponseEntity<?> sendMessage(@RequestBody MessageRequest request) {
-
         String result = messageService.processMessage(request);
 
         Map<String, String> response = new HashMap<>();
