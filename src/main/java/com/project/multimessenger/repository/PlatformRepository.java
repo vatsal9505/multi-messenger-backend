fb5b9005033dd3c62f27753980037f85d67a4567
Vatsal Kurhade <stonedelmo09@gmail.com>
2026-04-23 10:38:24 +0530

    fixed platform search ignore case

--- src/main/java/com/project/multimessenger/repository/PlatformRepository.java
+++ src/main/java/com/project/multimessenger/repository/PlatformRepository.java
@@ -3,9 +3,13 @@ package com.project.multimessenger.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.project.multimessenger.model.Platform;
 
+@Repository
 public interface PlatformRepository extends JpaRepository<Platform, Long> {
+
     Optional<Platform> findByPlatformNameIgnoreCase(String platformName);
+
 }
